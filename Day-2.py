71c6f0af9bb7623a5b8331f76279b53f8aac9b33
Bigyandutt Panda <46163157+BigyanduttPanda@users.noreply.github.com>
2025-05-29 15:28:40 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,9 @@
+#Calculate the area of a rectangle using user-input length and width
+
+Length = float(input("Enter the Length of the Rectangle: "))
+Width = float(input("Enter the Width of the Rectangle: "))
+
+Area = int(Length * Width)
+print("Area of the Rectangle is: ",Area);
+
+
